a45f3f25127798cef65fd940721de0604a658542
Guillaume Lebegue <lebegue.guillaume26@gmail.com>
2026-01-15 19:03:17 +0100

    final commit

--- plugins/blocks-gamestore/build/block-header/index.asset.php
+++ plugins/blocks-gamestore/build/block-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '83e9e4c21a59c08d1eb7');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'a6eb3fa8e988f418be04');
