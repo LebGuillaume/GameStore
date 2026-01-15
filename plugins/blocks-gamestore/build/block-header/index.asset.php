5f28f3bd60fef95a73fdd4d102b4f31f17de8371
Guillaume Lebegue <lebegue.guillaume26@gmail.com>
2026-01-15 14:16:48 +0100

    merge change

--- plugins/blocks-gamestore/build/block-header/index.asset.php
+++ plugins/blocks-gamestore/build/block-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '8a57a58900d20fd545c0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '83e9e4c21a59c08d1eb7');
