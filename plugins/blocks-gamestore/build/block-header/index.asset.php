244b9f44f4a4e65c826dbdb162d9c1c8c453e4e9
Guillaume Lebegue <lebegue.guillaume26@gmail.com>
2026-01-15 14:08:44 +0100

    Intégration du block-header

---
+++ plugins/blocks-gamestore/build/block-header/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '8a57a58900d20fd545c0');
--- themes/gamestore/functions.php
+++ themes/gamestore/functions.php
@@ -1,26 +1,45 @@
 <?php
-/**
- * Functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package gamestore
- * @since 1.0.0
- */
 
-/**
- * Enqueue the CSS files.
- *
- * @since 1.0.0
- *
- * @return void
- */
-function gamestore_styles() {
+function gamestore_styles()
+{
 	wp_enqueue_style(
-		'gamestore-style',
-		get_stylesheet_uri(),
+		'gamestore-general',
+		get_stylesheet_directory_uri() . '/assets/css/gamestore.css',
 		[],
-		wp_get_theme()->get( 'Version' )
+		wp_get_theme()->get('Version')
+	);
+	wp_enqueue_script(
+		'gamestore-general',
+		get_stylesheet_directory_uri() . '/assets/js/gamestore-theme-related.js',
+		[],
+		wp_get_theme()->get('Version'),
+		true
 	);
 }
-add_action( 'wp_enqueue_scripts', 'gamestore_styles' );
+add_action('wp_enqueue_scripts', 'gamestore_styles');
+
+function gamestore_googlge_font()
+{
+	$font_url = '';
+	$font_name = 'Urbanist';
+	$font_extra = 'ital,wght@0,400;0,700;1,400;1,700';
+
+	if ('off' !== _x('on', 'Google font: on or off', 'gamestore')) {
+		$query_args = array(
+			'family' => urldecode($font_name . ':' . $font_extra),
+			'subset' => urldecode('latin,latin-ext'),
+			'display' => urldecode('swap'),
+		);
+		$font_url = add_query_arg(
+			$query_args,
+			'https://fonts.googleapis.com/css'
+		);
+	}
+	return $font_url;
+}
+
+function gamestore_google_font_script()
+{
+	wp_enqueue_style('gamestore-google-font', gamestore_googlge_font(), [], '1.0.0');
+}
+add_action('wp_enqueue_scripts', 'gamestore_google_font_script');
